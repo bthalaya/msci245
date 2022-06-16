fcbb30cc578a4400b6f6e542bf1a3c58eeb44c8b
bthalaya <bthalaya@uwaterloo.ca>
2022-06-16 19:44:38 -0400

    first commit

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,171 +2,166 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import { MuiThemeProvider, createTheme, ThemeProvider, styled} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-
-
+import { FormControl } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { InputLabel } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+import { FormHelperText } from '@material-ui/core';
+import {makeStyles} from "@material-ui/core/styles"
+import { Box } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+import Grid from "@material-ui/core/Grid";
+ 
+ 
 //Dev mode
 const serverURL = "https://ov-research-4.uwaterloo.ca:9090"; //enable for dev mode
-
+ 
 //Deployment mode instructions
 //const serverURL = "http://ov-research-4.uwaterloo.ca:PORT"; //enable for deployed mode; Change PORT to the port number given to you;
-//To find your port number: 
-//ssh to ov-research-4.uwaterloo.ca and run the following command: 
+//To find your port number:
+//ssh to ov-research-4.uwaterloo.ca and run the following command:
 //env | grep "PORT"
 //copy the number only and paste it in the serverURL in place of PORT, e.g.: const serverURL = "http://ov-research-4.uwaterloo.ca:3000";
-
-const fetch = require("node-fetch");
-
+ 
 const opacityValue = 0.9;
 
-const theme = createTheme({
+const useStyles = makeStyles((theme)=> ({
+ rootReviewTitle: {
+  margin: theme.spacing(1),
+  width: 200,
+},
+ rootReviewBody: {
+  margin: theme.spacing(1),
+  width: 200,
+ }
+})
+)
+
+const classes = useStyles()
+
+const lightTheme = createTheme({
   palette: {
-    type: 'dark',
+    type: 'light',
     background: {
-      default: "#000000"
+      default: "#ffffff"
     },
     primary: {
-      main: "#52f1ff",
+      main: '#ef9a9a',
+      light: '#ffcccb',
+      dark: '#ba6b6c',
+      background: '#eeeeee'
     },
     secondary: {
-      main: "#b552f7",
+      main: "#b71c1c",
+      light: '#f05545',
+      dark: '#7f0000'
     },
   },
 });
 
-const styles = theme => ({
-  root: {
-    body: {
-      backgroundColor: "#000000",
-      opacity: opacityValue,
-      overflow: "hidden",
-    },
-  },
-  mainMessage: {
-    opacity: opacityValue,
-  },
+const MainGridContainer = styled(Grid)(({ theme }) => ({
+  margin: theme.spacing(4),
+}))
+ 
+function App (){
 
-  mainMessageContainer: {
-    marginTop: "20vh",
-    marginLeft: theme.spacing(20),
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: theme.spacing(4),
-    },
-  },
-  paper: {
-    overflow: "hidden",
-  },
-  message: {
-    opacity: opacityValue,
-    maxWidth: 250,
-    paddingBottom: theme.spacing(2),
-  },
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box
+        sx={{
+          height: '100vh',
+          opacity: opacityValue,
+          overflow: "hidden",
+          backgroundColor: lightTheme.palette.background.default,
 
-});
-
-
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userID: 1,
-      mode: 0
-    }
-  };
-
-  componentDidMount() {
-    //this.loadUserSettings();
-  }
-
-
-  loadUserSettings() {
-    this.callApiLoadUserSettings()
-      .then(res => {
-        //console.log("loadUserSettings returned: ", res)
-        var parsed = JSON.parse(res.express);
-        console.log("loadUserSettings parsed: ", parsed[0].mode)
-        this.setState({ mode: parsed[0].mode });
-      });
-  }
-
-  callApiLoadUserSettings = async () => {
-    const url = serverURL + "/api/loadUserSettings";
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        //authorization: `Bearer ${this.state.token}`
-      },
-      body: JSON.stringify({
-        userID: this.state.userID
-      })
-    });
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    console.log("User settings: ", body);
-    return body;
-  }
-
-  render() {
-    const { classes } = this.props;
-
-
-
-    const header = (
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        justify="flex-start"
-        alignItems="flex-start"
-        style={{ minHeight: '100vh' }}
-        className={classes.headerContainer}
+        }}
       >
-        <Grid item>
-
-          <Typography
-            variant={"h3"}
-            className={classes.header}
-            align="flex-start"
-          >
-            {this.state.mode === 0 ? (
-              <React.Fragment>
-                Review a Movie!
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                Welcome back!
-              </React.Fragment>
-            )}
+        <MainGridContainer
+          container
+          spacing={1}
+          style={{ maxWidth: '50%' }}
+          direction="column"
+          justify="flex-start"
+          alignItems="stretch"
+        >
+          <Typography variant="h3" gutterBottom component="div">
+            Movie Review
           </Typography>
 
-        </Grid>
-      </Grid>
-    )
-
-
-    return (
-      <MuiThemeProvider theme={theme}>
-        <div className={classes.root}>
-          <CssBaseline />
-          <Paper
-            className={classes.paper}
-          >
-            {header}
-          </Paper>
-
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+          <MovieSelection/>
+          <hr></hr>
+          <ReviewTitle/>
+          <hr></hr>
+          <ReviewBody/>
+          <hr></hr>
+        </MainGridContainer>
+      </Box>
+    </ThemeProvider>
+  );
 }
-
-Home.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Home);
+ 
+function MovieSelection () {
+ 
+ const [movie, setMovie] = React.useState('');
+ 
+ const handleChange = (event) => {
+   setMovie(event.target.movie);
+ };
+ 
+ return (
+    <form className={classes.formControl} noValidate autoComplete='off'>
+     <FormControl>
+       <InputLabel id="demo-simple-select-label">Movie</InputLabel>
+       <Select
+         labelId="demo-simple-select-label"
+         id="demo-simple-select"
+         movie={movie}
+         label="Movie"
+         onChange={handleChange}
+       >
+         <MenuItem movie={'Footloose'}>Footloose</MenuItem>
+         <MenuItem movie={'The Karate Kid'}>The Karate Kid</MenuItem>
+         <MenuItem movie={'The Notebook'}>The Notebook</MenuItem>
+         <MenuItem movie={'Forrest Gump'}>Forrest Gump</MenuItem>
+         <MenuItem movie={'Top Gun'}>Top Gun</MenuItem>
+       </Select>
+       <FormHelperText>Select a Movie to Review</FormHelperText>
+     </FormControl>
+ );
+}
+ 
+function ReviewTitle(){
+  const classes = useStyles();
+ return (
+   <form className={useStyles.rootReviewTitle} noValidate autoComplete='off'>
+     <div>
+       <TextField required id="standard-required" label="Required" defaultValue="Movie Review Title" helperText="Enter Movie Review Title" />
+     </div>
+   </form>
+ );
+ }
+ 
+function ReviewBody(){
+ const classes = useStyles();
+ 
+ return (
+   <form className={useStyles.rootReviewBody} noValidate autoComplete='off'>
+     <div>
+     <TextField
+         id="outlined-multiline-static"
+         label="Movie Review"
+         multiline
+         rows={4}
+         defaultValue="Default Value"
+         variant="outlined"
+         helperText="Enter Movie Review"
+       />
+     </div>
+   </form>
+ );
+}
+ 
+export default App;
